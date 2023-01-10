33ce8527e2da8549ffde0acbde4b29e68f3d300d
y-double-m <75093446+y-double-m@users.noreply.github.com>
2023-01-10 17:33:21 +0900

    Add files via upload

--- myweb/run.py
+++ myweb/run.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import requests
+import json
 
 app = Flask(__name__)
 
@@ -30,16 +31,25 @@ def request_code():
     }
     
     url1 = 'https://fluterepair.uplus.co.kr:8443/openapi/appinfo'
-    url2 = 'https://uremocon.com/pairing/getPairingList'
-    url3 = 'https://google.com'
+    url2 = 'https://fluterepair.uplus.co.kr:8443/openapi/appList'
+    url3 = 'https://uremocon.com/pairing/getPairingList'
+    url4 = 'https://google.com'
     
-    response1 = requests.post(url1, headers=headers, json=json_data1, verify=False)    
-    response2 = requests.post(url3, params=params, headers=headers, verify=False)
+    response1 = requests.post(url1, headers=headers, json=json_data1, verify=False)
+    response2 = requests.post(url2, headers=headers, json=json_data2, verify=False)    
+    response3 = requests.get(url3, params=params, headers=headers, verify=False)
     
     state1 = response1.status_code
     state2 = response2.status_code
+    state3 = response3.status_code
     
-    return render_template("index.html", url1=url1, url2=url2, state1=state1, state2=state2)
+    text1 = response1.json()
+    text2 = response2.json()
+    text3 = response3.json()
+    
+    return render_template("index.html", url1=url1, url2=url2, url3=url3, 
+                           state1=state1, state2=state2, state3=state3,
+                           text1=text1, text2=text2, text3=text3)
           
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
